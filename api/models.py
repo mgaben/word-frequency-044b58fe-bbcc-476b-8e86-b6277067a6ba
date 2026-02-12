d9f33b3f283a520b8a4469625c50d5c0341fb97f
Gabor Majoros <juvefanmaster@gmail.com>
2026-02-12 16:24:57 +0100

    Add request and response models

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Dict
+
+
+class KeywordsRequest(BaseModel):
+    article: str
+    depth: int
+    ignore_list: list[str]
+    percentile: int
+
+
+class WordFrequency(BaseModel):
+    word_count: Dict[str, int]
+    word_percentage: Dict[str, float]
